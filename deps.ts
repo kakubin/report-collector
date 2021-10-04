e329a5c43188c063bbdc3cbef46d26444e1d0c5b
kakubin <wetsand.wfs@gmail.com>
2021-10-04 15:50:17 +0900

    fix version of denodb

--- deps.ts
+++ deps.ts
@@ -4,5 +4,5 @@ export {
   DataTypes,
   Model,
   Relationships,
-} from "https://deno.land/x/denodb/mod.ts";
+} from "https://deno.land/x/denodb@v1.0.39/mod.ts";
 export * from "https://deno.land/x/std@0.108.0/io/mod.ts";
